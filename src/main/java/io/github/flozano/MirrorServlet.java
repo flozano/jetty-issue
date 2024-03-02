df99bf2f410e98ecf52d85840ff7baa1e6b411fb
Francisco A. Lozano <flozano@gmail.com>
2024-03-02 13:52:09 +0100

    Add read all before writing test

--- src/main/java/io/github/flozano/MirrorServlet.java
+++ src/main/java/io/github/flozano/MirrorServlet.java
@@ -26,16 +26,17 @@ public class MirrorServlet extends HttpServlet {
 		resp.setContentType(req.getContentType());
 		resp.setContentLength(req.getContentLength());
 		resp.setCharacterEncoding(req.getCharacterEncoding());
-		LOGGER.info("Copying input to output");
+		LOGGER.info("Copying input to output (bufferSize={})", bufferSize);
 		resp.setStatus(200);
-		// Works
-		//byte[] content = IOUtils.toByteArray(req.getInputStream());
-		//IOUtils.write(content, resp.getOutputStream());
-		// Fails with timeout.
-		byte[] buffer = new byte[bufferSize];
-		try(var inputStream = req.getInputStream()) {
-			try(var outputStream = resp.getOutputStream()) {
-				copyLarge(inputStream, outputStream, buffer);
+		try (var inputStream = req.getInputStream()) {
+			try (var outputStream = resp.getOutputStream()) {
+				if (bufferSize == 0) {
+					var content = req.getInputStream().readAllBytes();
+					resp.getOutputStream().write(content);
+				} else {
+					byte[] buffer = new byte[bufferSize];
+					copyLarge(inputStream, outputStream, buffer);
+				}
 			}
 		}
 		LOGGER.info("Finished request: {}", req.getRequestURI());
@@ -43,6 +44,7 @@ public class MirrorServlet extends HttpServlet {
 
 	public static long copyLarge(final InputStream inputStream, final OutputStream outputStream, final byte[] buffer)
 			throws IOException {
+		// copied from commons-io, just added log statements
 		Objects.requireNonNull(inputStream, "inputStream");
 		Objects.requireNonNull(outputStream, "outputStream");
 		long count = 0;
@@ -50,10 +52,11 @@ public class MirrorServlet extends HttpServlet {
 		while (EOF != (n = inputStream.read(buffer))) {
 			outputStream.write(buffer, 0, n);
 			count += n;
-			LOGGER.info("Copied {} bytes (count={})",n, count);
+			LOGGER.info("Copied {} bytes (count={})", n, count);
 		}
 		return count;
 	}
+
 	public static final int EOF = -1;
 
 }
--- src/test/java/io/github/flozano/jetty/JettyIssue.java
+++ src/test/java/io/github/flozano/jetty/JettyIssue.java
@@ -8,7 +8,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -19,7 +18,7 @@ import io.github.flozano.ServerConfiguration;
 public class JettyIssue {
 
 	@ParameterizedTest
-	@ValueSource(ints = { 1024, 8192, 1024 * 1024, 2 * 1024 * 1024 })
+	@ValueSource(ints = { 0, 1024, 8192, 1024 * 1024, 4 * 1024 * 1024 })
 	public void test(int copyBufferSize) throws IOException, InterruptedException {
 		try (JettyServer server = new JettyServer(new ServerConfiguration())) {
 			server.configureServlet("mirror", new MirrorServlet(copyBufferSize));
