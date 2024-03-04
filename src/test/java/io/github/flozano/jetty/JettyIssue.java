a9734606fa054b220413d2698bde8caf8a51d1ea
Francisco A. Lozano <flozano@gmail.com>
2024-03-04 21:38:56 +0100

    Fix future listener size

--- src/test/java/io/github/flozano/jetty/JettyIssue.java
+++ src/test/java/io/github/flozano/jetty/JettyIssue.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.eclipse.jetty.client.FutureResponseListener;
 import org.eclipse.jetty.client.StringRequestContent;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -67,7 +68,9 @@ public class JettyIssue {
 				var request = httpClient.newRequest(server.getBaseURI()).method("POST").body(
 						new StringRequestContent("application/octet-stream",
 								RandomStringUtils.randomAlphanumeric(requestAndResponseSize), StandardCharsets.UTF_8));
-				var response = request.send();
+				var listener = new FutureResponseListener(request, 5_000_000);
+				request.send(listener);
+				var response = listener.get();
 				assertEquals(200, response.getStatus());
 			} finally {
 				httpClient.stop();
